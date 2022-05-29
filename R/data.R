f13717718dc1b11caba704cf87b2c3b6091e15a8
jianjunwang <jjwang@niglas.ac.cn>
2022-05-29 18:13:53 +0800

    iDOM-V0.1.0

--- R/data.R
+++ R/data.R
@@ -1,24 +1,26 @@
 #' Microbial community data
 #'
-#' Example data of a bacterial compositional table (50 samples by 100 bacterial species)
+#' Example data of a Microbial compositional table (50 samples by 100 bacterial species)
 #' 
-#' @format The format for community data is a data.frame with samples in the rows and species in the columns. 
-#' @references Wang J, Pan F, Soininen J, et al. Nutrient enrichment modifies temperature-biodiversity relationships in large-scale field experiments. 
-#' \emph{Nature Communications}, 2016, 7(1): 1-9.
-#' @references Hu A, Choi M, Tanentzap AJ, Liu J, Jang K-S, Lennon JT, et al.
-#' Quantifying the associations between dissolved organic matter and microbes under global change. \emph{Nature communications} 2022.
+#' @format The format for Microbial community data is a data.frame with samples in the rows and species in the columns. 
+#' @references Wang J, Pan F, Soininen J, Heino J, Shen J. 2016. 
+#' Nutrient enrichment modifies temperature-biodiversity relationships in large-scale field experiments. \emph{Nature Communications} 7:1-9.
+#' @references Hu A, Choi M, Tanentzap AJ, Liu J, Jang K-S, Lennon JT, Liu Y, Soininen J, Lu X, Zhang Y, Shen J, Wang J. 2021.
+#' Quantifying microbial associations of dissolved organic matter under global change. \emph{bioRxiv}.
+#' @references Hu A, Choi M, Tanentzap AJ, Liu J, Jang K-S, Lennon JT, Liu Y, Soininen J, Lu X, Zhang Y, Shen J, Wang J. 2022.
+#' Quantifying the associations between dissolved organic matter and microbes under global change. \emph{Nature communications}.
 #' @source \url{https://doi.org/10.1038/ncomms13960}.
 #' @examples data(Microbial.data)
 "Microbial.data"
-
 
 #' DOM molecules data
 #'
 #' Example data of a DOM compositional table (50 samples by 100 DOM molecules)
 #'
 #' @format The format for DOM data is a data.frame with samples in the rows and DOM molecules in the columns.
-#' @references 
-#' @references Hu A, Choi M, Tanentzap AJ, Liu J, Jang K-S, Lennon JT, et al.
-#' Quantifying the associations between dissolved organic matter and microbes under global change. \emph{Nature communications} 2022.
+#' @references Hu A, Choi M, Tanentzap AJ, Liu J, Jang K-S, Lennon JT, Liu Y, Soininen J, Lu X, Zhang Y, Shen J, Wang J. 2021.
+#' Quantifying microbial associations of dissolved organic matter under global change. \emph{bioRxiv}.
+#' @references Hu A, Choi M, Tanentzap AJ, Liu J, Jang K-S, Lennon JT, Liu Y, Soininen J, Lu X, Zhang Y, Shen J, Wang J. 2022.
+#' Quantifying the associations between dissolved organic matter and microbes under global change. \emph{Nature communications}.
 #' @examples data(DOM.data)
 "DOM.data"
